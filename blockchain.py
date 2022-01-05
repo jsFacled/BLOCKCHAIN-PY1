b3a3e93f9e48862755b0304c8268d60a1a22b821
jsFacled <facled@gmail.com>
2022-01-05 20:11:28 -0300

    creando la clase Blockchain

--- blockchain.py
+++ blockchain.py
@@ -2,12 +2,29 @@ from hashlib import sha256
 import json
 
 Class Block:
-    def __init__(self, index, transactions, timestamp):
+    def __init__(self, index, transactions, timestamp, previous_hash):
         self.index = index
         self.transactions = transactions
         self.timestamp = timestamp
+        self.previous_hash = previous_hash
 
 def compute_hash(block):
     block_string = json.dump(self.__dict__, sort = True)
     return sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
     
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), "0"*64)
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+        
+        
