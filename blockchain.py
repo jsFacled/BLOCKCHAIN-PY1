d2bd573e52a743f0c85f42d47b5752c21cfba718
jsFacled <facled@gmail.com>
2022-01-05 19:42:20 -0300

    método para computar el hash

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,13 @@
 from hashlib import sha256
 import json
 
-Class block:
+Class Block:
     def __init__(self, index, transactions, timestamp):
         self.index = index
         self.transactions = transactions
         self.timestamp = timestamp
 
+def compute_hash(block):
+    block_string = json.dump(self.__dict__, sort = True)
+    return sha256(block_string.encode()).hexdigest()
+    
