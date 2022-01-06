489b43e4c589b4730aefa52569c47206fe1de86a
jsFacled <facled@gmail.com>
2022-01-06 17:16:25 -0300

    Segunda Parte - Ahora a minargit add .

--- blockchain.py
+++ blockchain.py
@@ -1,43 +1,94 @@
-
 from hashlib import sha256
-import json
 import time
+import json
 
 class Block:
+
     def __init__(self, index, transactions, timestamp, previous_hash):
         self.index = index
         self.transactions = transactions
         self.timestamp = timestamp
         self.previous_hash = previous_hash
 
+
     def compute_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys = True)
         return sha256(block_string.encode()).hexdigest()
+
+
 class Blockchain:
+
+    difficulty = 2
+
     def __init__(self):
         self.unconfirmed_transactions = []
         self.chain = []
         self.create_genesis_block()
-    
+
     def create_genesis_block(self):
-        genesis_block = Block(0, [], time.time(), "0"*64)
+        genesis_block = Block(0, [], time.time(), '0'*64)
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
-    
+        
     @property
     def last_block(self):
         return self.chain[-1]
-        
+    
     def print_block(self, n):
         if (len(self.chain) < n):
             return
-        else:
-            block = self.chain(n)
-            return "\n Index: {}\n Transactions: {}\n Timestamp: {}\n PreviousHash: {}\n ".format(block.index, block.transactions, block.timestamp, block.previous_hash)
+        else:      
+            block = self.chain[n]
+            return ' Index {}\n Transactions {}\n Timestamp: {}\n PreviousHash: {}\n'.format(block.index, block.transactions, block.timestamp, block.previous_hash) 
 
-#---Pruebo la Blockchain---
+
+    def proof_of_work(self, block):
+        block.nonce = 0
+        compute_hash = block.compute_hash()
+        while not compute_hash.startswith('0'*Blockchain.difficulty):
+            block.nonce += 1
+            compute_hash = block.compute_hash()
+            print(compute_hash)
+        return compute_hash
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+        if (previous_hash != block.previous_hash):
+            return False
+        if not self.is_valid_proof(block, proof):
+            return False
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and block_hash == block.compute_hash())
+
+    def new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
+        last_block = self.last_block
+        new_block = Block(index = last_block.index + 1, transactions = self.unconfirmed_transactions, timestamp = time.time(), previous_hash = last_block.hash)
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+""" --- probando la minería --- """
 
 a = Blockchain()
-print (a.print_block(0))
+a.new_transaction('transaccion 1')
+a.new_transaction('transaccion 2')
+a.mine()
+a.new_transaction('transaccion 3')
+a.mine()
+a.new_transaction('transaccion 4')
+a.mine()
+len_bc = len(a.chain)
+print(a.print_block(len_bc - 1))
+
 
         
